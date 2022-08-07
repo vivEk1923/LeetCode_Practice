652ddc5d960d2998f5079de183cd6ca0ed4efc30
Vivek Kumar Singh <vivek11singh1@gmail.com>
2022-08-08 00:18:49 +0530

    Update

---
+++ 278.First_Bad_version.cpp
@@ -0,0 +1,25 @@
+// The API isBadVersion is defined for you.
+ bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+         
+        long int low = 1, high = n;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low)  / 2;
+                
+              
+                
+               if(isBadVersion(mid))
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+                if(isBadVersion(mid) && !(isBadVersion(mid-1)))
+                    return mid;
+        }
+        return 1;        
+    }
+};
